341b0d9732e3b27619be15761277cfb501b5c177
hassadem <hassadem001@gmail.com>
2023-07-20 15:47:51 +0100

    updated

--- jewelry.js
+++ jewelry.js
@@ -1,10 +1,12 @@
 // Cart functionality
-const cartItems = [];
+let cartItems = [];
 const cartCount = document.querySelector('.cart-count');
+const cartIcon = document.querySelector('#cartIcon');
 const cartList = document.querySelector('.cart-items');
 
 // Fetch products from the API and display them on the homepage
-fetch('https://fakestoreapi.com/products/category/jewelery')
+if (document.querySelector('.product-list')) {
+  fetch('https://fakestoreapi.com/products/category/jewelery')
   .then(response => response.json())
   .then(products => {
     const productContainer = document.querySelector('.product-list');
@@ -39,6 +41,16 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
   })
   .catch(error => console.error('Error fetching products:', error));
 
+}
+
+if (cartIcon) {
+  cartIcon.addEventListener('click', () => {
+    // Pass the cartItems data to the checkout.html page via the URL parameter
+    const queryParams = new URLSearchParams({ cart: JSON.stringify(cartItems) });
+    const checkoutURL = `checkout.html?${queryParams.toString()}`;
+    window.location.href = checkoutURL;
+  });
+}
 
 function addToCart(product) {
   cartItems.push(product);
@@ -47,22 +59,56 @@ function addToCart(product) {
 
 function updateCart() {
   cartCount.textContent = cartItems.length;
-  cartList.innerHTML = '';
+  saveCartToLocalStorage();
+  
+  if (cartList) {
+    cartList.innerHTML = '';
 
-  cartItems.forEach(item => {
-    const cartItem = document.createElement('li');
-    cartItem.textContent = `${item.title} - $${item.price.toFixed(2)}`;
-    cartList.appendChild(cartItem);
-  });
+    cartItems.forEach(item => {
+      const cartItem = document.createElement('li');
+      cartItem.textContent = `${item.title} - $${item.price.toFixed(2)}`;
+      cartList.appendChild(cartItem);
+    });
+  }
+}
+
+function saveCartToLocalStorage() {
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
+
+// Function to update the cart list on the checkout.html page
+function updateCartOnCheckout() {
+  if (cartList) {
+    cartList.innerHTML = '';
+
+    cartItems.forEach(item => {
+      const cartItem = document.createElement('li');
+      cartItem.textContent = `${item.title} - $${item.price.toFixed(2)}`;
+      cartList.appendChild(cartItem);
+    });
+  }
+}
+
+const storedCartItems = JSON.parse(localStorage.getItem('cartItems'));
+if (Array.isArray(storedCartItems)) {
+  cartItems = storedCartItems;
+  updateCart();
+  updateCartOnCheckout();
 }
 
 const checkoutBtn = document.querySelector('.checkout-btn');
-checkoutBtn.addEventListener('click', () => {
-  if (cartItems.length === 0) {
-    alert('Your cart is empty. Add some products before checkout.');
-  } else {
-    alert('Thank you for your purchase!');
-    cartItems.length = 0;
-    updateCart();
-  }
-});
+
+// Check if the checkout button exists before adding the event listener
+if (checkoutBtn) {
+  checkoutBtn.addEventListener('click', () => {
+    if (cartItems.length === 0) {
+      alert('Your cart is empty. Add some products before checkout.');
+    } else {
+      alert('Thank you for your purchase!');
+      cartItems = [];
+      updateCart();
+      updateCartOnCheckout();
+    }
+  });
+}
+updateCart();
